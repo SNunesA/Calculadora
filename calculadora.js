b5da976253afc26ac5167e7727169eb89d0fc539
SnunesA <snunesadiers@gmail.com>
2023-02-15 23:11:55 -0300

    ultimo

--- calculadora.js
+++ calculadora.js
@@ -1,32 +1,92 @@
+// pega o conteudo do input
 const campo = document.getElementById("calculo");
 
-const simbolos = ["+", "-", "/", "*", "**"];
+/**
+ * Estrutura do historico
+ * {
+ *   tipo: "alterar" ou "limpar" ou "calcular",
+ *   valorFinal: string
+ * }
+ */
+const historico = [];
 
-function adicionarValor(valor) {
-  const valorAtual = campo.value;
+const operadores = ["+", "-", "/", "*", "**"];
+
+function adicionarValor(valorAdicionar) {
+  let valorAtual = campo.value;
 
   // permitir apenas que o primeiro valor seja número
-  if (valorAtual == "" && simbolos.includes(valor)) {
+  if (valorAtual == "" && operadores.includes(valorAdicionar)) {
     return;
   }
-
-  // não permitir simbolos seguidos
-  // https://horadecodar.com.br/2021/11/05/como-pegar-o-ultimo-caractere-de-uma-string-em-javascript/
+  // pega o ultimo caracter atraves do slice -1
   const ultimoCaracter = valorAtual.slice(-1);
-  if (simbolos.includes(ultimoCaracter) && simbolos.includes(valor)) {
+
+  // não permitir operadores seguidos
+  if (
+    operadores.includes(ultimoCaracter) &&
+    operadores.includes(valorAdicionar)
+  ) {
     return;
   }
 
-  campo.value = `${valorAtual}${valor}`;
+  // não permitir que o primeiro caracter seja ponto
+  if (ultimoCaracter === "" && valorAdicionar === ".") {
+    return;
+  }
+
+  // permitir pontos apenas após números
+  if (operadores.includes(ultimoCaracter) && valorAdicionar === ".") {
+    return;
+  }
+
+  // não permitir pontos seguidos
+  if (ultimoCaracter === "." && valorAdicionar === ".") {
+    return;
+  }
+
+  // se a ultima ação do histórico for "calcular", substitui o valor do campo por valorAdicionar
+  const ultimoItemHistorico = historico[historico.length - 1];
+  if (ultimoItemHistorico && ultimoItemHistorico.tipo === "calcular") {
+    // e que o valor digitado não seja operador
+    if (operadores.includes(valorAdicionar) === false) {
+      valorAtual = "";
+    }
+  }
+  // concatena
+  const novoValor = `${valorAtual}${valorAdicionar}`;
+  // joga o valor novo no input
+  campo.value = novoValor;
+
+  historico.push({
+    tipo: "alterar",
+    valorFinal: novoValor,
+  });
 }
 
 function limpar() {
   campo.value = "";
+  historico.push({
+    tipo: "limpar",
+    valorFinal: "",
+  });
 }
 
 function calcular() {
-  const valorAtual = campo.value;
-  campo.value = eval(valorAtual);
+  let valorAtual = campo.value;
+  const ultimoCaracter = valorAtual.slice(-1);
+
+  // se o ultimo caracter for um operador, remove ele do calculo
+  if (operadores.includes(ultimoCaracter)) {
+    valorAtual = valorAtual.slice(0, -1);
+  }
+
+  const resultado = eval(valorAtual);
+  campo.value = resultado;
+  historico.push({
+    tipo: "calcular",
+    valorFinal: resultado,
+  });
 }
 
 /**
